52c76239d78b6c29095e752dc0c093fbc2cb01ec
DoglasRocha <doglas_rocha@protonmail.ch>
2021-09-07 00:05:07 -0300

    end of the seventh class

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
 
 const handleKeyUp = event => {
-    if (event.keyCode === 32) {
+    if ((event.keyCode === 32) && (!isJumping)) {
         jump();
     }
 }
 
 const jump = () => {
-    let position = 0;
+    isJumping = true;
 
     let upInterval = setInterval(() => {
         
@@ -19,6 +22,7 @@ const jump = () => {
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                 position -= 20;
                 dino.style.bottom = position + 'px';
@@ -33,4 +37,30 @@ const jump = () => {
         
 }
 
-document.addEventListener('keyup', handleKeyUp)
+const createCactus = () => {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+             clearInterval(leftInterval);
+             document.body.innerHTML = '<h1 class="game-over">Game Over</h1>'
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keydown', handleKeyUp);
